a296029de824d1130a1cecfe380167df8a991b18
zhifeng ou <zhifengou@hotmail.com>
2018-10-13 02:29:44 +0000

    automatically backed up by learn

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
    def hello_world
-    
+    "Hello World"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +56,5 @@ Rails.application.routes.draw do
   #   end
   
   get 'hello_world', to: 'static#hello_world'
+
 end
